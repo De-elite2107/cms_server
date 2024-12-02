4efb5165f9f9aec49baad41a3ab38a28e525bde1
theroyalimage <delightadediran21@gmail.com>
2024-12-02 14:39:03 +0100

    settingd

--- cms/settings.py
+++ cms/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cmsserver-production.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['cmsserver.up.railway.app', 'localhost']
 
 
 # Application definition
