1478c0a7b0184b3f22c57c1f8d25926daddd1b3f
theroyalimage <delightadediran21@gmail.com>
2024-12-29 12:59:17 +0100

    Update application code

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import os
 import psycopg2
 
 conn = psycopg2.connect(
-    host=os.getenv('DB_HOST'),
-    database=os.getenv('DB_NAME'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PWD')
+    host=os.getenv('PGHOST'),
+    database=os.getenv('PGDATABASE'),
+    user=os.getenv('PGUSER'),
+    password=os.getenv('PGPASSWORD')
 )
