cc7c05dca82e885f626d70ba8348a159f5cdcc70
theroyalimage <delightadediran21@gmail.com>
2024-12-30 16:10:34 +0100

    li

--- cms/settings.py
+++ cms/settings.py
@@ -123,7 +123,13 @@ DATABASES = {
     }
 }
 
-
+# Alternatively, if you're using DATABASE_URL directly
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
