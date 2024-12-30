4ff613dbb25b7950d5159dd6c9763f225b7323c2
theroyalimage <delightadediran21@gmail.com>
2024-12-30 17:10:25 +0100

    link

--- cms/settings.py
+++ cms/settings.py
@@ -111,25 +111,9 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'), 
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('PGHOST'),
-        'PORT': os.getenv('PGPORT'),   
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
-# Alternatively, if you're using DATABASE_URL directly
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
